657e490e6c28049012a5b9b970024e0396967638
Tan Siewert <tan@siewert.io>
2024-12-26 19:07:26 +0100

    bastelpi: fix indentation for ssh socket
    Signed-off-by: Tan Siewert <tan@siewert.io>

--- users/sinuscosinustan/zsh.nix
+++ users/sinuscosinustan/zsh.nix
@@ -32,13 +32,12 @@
       };
 
       initExtra = "
-        export SSH_AUTH_SOCK=~/.ssh/ssh-agent.$(hostname).sock
-        ssh-add -l 2>/dev/null >/dev/null
-        if [ $? -ge 2 ]; then
-          ssh-agent -a \"$SSH_AUTH_SOCK\" >/dev/null
-        fi
+export SSH_AUTH_SOCK=~/.ssh/ssh-agent.$(hostname).sock
+ssh-add -l 2>/dev/null >/dev/null
+if [ $? -ge 2 ]; then
+  ssh-agent -a \"$SSH_AUTH_SOCK\" >/dev/null
+fi
       ";
-
 
       oh-my-zsh = {
         enable = true;
